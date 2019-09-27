46ba320ed95052247e51ff6de988ae87af70874a
icamys <prisacari.dmitrii@gmail.com>
2019-09-27 16:09:01 +0300

    added json repr name for Record.ErrCode

--- record.go
+++ record.go
@@ -2,7 +2,7 @@ package whoisparser
 
 // Record structure with info about registars and registants
 type Record struct {
-	ErrCode    ErrCode
+	ErrCode    ErrCode     `json:"error_code,omitempty"`
 	Registrar  *Registrar  `json:"registrar,omitempty"`
 	Registrant *Registrant `json:"registrant,omitempty"`
 	Admin      *Registrant `json:"admin,omitempty"`
