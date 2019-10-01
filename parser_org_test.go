819d2e536416c9dfb2aaef07005b604e458d8df8
Prisacari Dmitrii <mrgangs1991@gmail.com>
2019-10-01 11:48:15 +0300

    Review and refactor of parser_org_test.go

--- parser_org_test.go
+++ parser_org_test.go
@@ -2,96 +2,79 @@ package whoisparser
 
 // TODO Rename file when more then 1 tld test added!
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"reflect"
 	"testing"
 )
 
-type DataForTest struct {
-	Field          string
+type AssertParams struct {
+	TargetField    string
 	ExpectedResult interface{}
-	CheckType      int
+	AssertType     int
 }
+
+type AssertParamsMap map[string][]*AssertParams
 
 const (
-	CONTAINS = iota //TODO CONTAINS can be unreliable, because return true when looking string in string.........
-	EQUAL
-	LEN
+	AssertContains = iota //TODO CONTAINS can be unreliable, because return true when looking string in string
+	AssertEqual
+	AssertLen
 )
 
-func TestStartParserTesters(t *testing.T) { //TODO RENAME
-
-	checkParams := map[string][]*DataForTest{
+func TestParserOrg(t *testing.T) {
+	assertParamsMap := AssertParamsMap{
 		"Registrar": {
-			{Field: "DomainName", ExpectedResult: "WIKIPEDIA.ORG", CheckType: EQUAL},
-			{Field: "DomainDNSSEC", ExpectedResult: "unsigned", CheckType: EQUAL},
-			{Field: "RegistrarName", ExpectedResult: "MarkMonitor Inc.", CheckType: EQUAL},
-			{Field: "WhoisServer", ExpectedResult: "whois.markmonitor.com", CheckType: EQUAL},
-			{Field: "NameServers", ExpectedResult: "NS2.WIKIMEDIA.ORG", CheckType: CONTAINS},
-			{Field: "Emails", ExpectedResult: "abusecomplaints@markmonitor.com", CheckType: EQUAL},
-			{Field: "RegistrarID", ExpectedResult: 3, CheckType: LEN},
+			{TargetField: "DomainName", ExpectedResult: "WIKIPEDIA.ORG", AssertType: AssertEqual},
+			{TargetField: "DomainDNSSEC", ExpectedResult: "unsigned", AssertType: AssertEqual},
+			{TargetField: "RegistrarName", ExpectedResult: "MarkMonitor Inc.", AssertType: AssertEqual},
+			{TargetField: "WhoisServer", ExpectedResult: "whois.markmonitor.com", AssertType: AssertEqual},
+			{TargetField: "NameServers", ExpectedResult: "NS2.WIKIMEDIA.ORG", AssertType: AssertContains},
+			{TargetField: "Emails", ExpectedResult: "abusecomplaints@markmonitor.com", AssertType: AssertEqual},
+			{TargetField: "RegistrarID", ExpectedResult: 3, AssertType: AssertLen},
 		},
 		"Registrant": {
-			{Field: "Organization", ExpectedResult: "Wikimedia Foundation, Inc.", CheckType: EQUAL},
+			{TargetField: "Organization", ExpectedResult: "Wikimedia Foundation, Inc.", AssertType: AssertEqual},
 		},
 	}
-	parserTester(
-		t,
-		orgParser,
-		checkParams,
-		"test_data/whois_org/wikipedia.org.txt",
-		".org parser",
-	)
 
+	testDataFilepath := "test_data/whois_org/wikipedia.org.txt"
+	parserName := ".org parser"
+
+	testParser(t, orgParser, assertParamsMap, testDataFilepath, parserName)
 }
-func parserTester(t *testing.T, targetParser *Parser, containParams map[string][]*DataForTest, pathToInput string, parserName string) {
+
+func testParser(t *testing.T, parser *Parser, assertParamsMap AssertParamsMap, inputFilepath, parserName string) {
 	var fileBytes []byte
 	var err error
 	var text string
-	var whoisInfo *Record
-	var reflectWhoisInfo reflect.Value
+	var whoisRecord *Record
+	var whoisRecordReflect reflect.Value
+	var assertMsg string
+	var assertMsgFormat = "failed on \"%s\"\nstructure \"%s.%s\"\npath to input file: \"%s\""
 
-	fileBytes, err = ioutil.ReadFile(pathToInput)
+	fileBytes, err = ioutil.ReadFile(inputFilepath)
 	assert.NoError(t, err, "failed to open file with test data")
 
 	text = string(fileBytes)
 
-	whoisInfo = targetParser.Parse(text)
-	reflectWhoisInfo = reflect.ValueOf(whoisInfo).Elem()
-	for parserStruckField, TestDataStructs := range containParams {
-		for _, TestData := range TestDataStructs {
-			filedForCheck := reflectWhoisInfo.FieldByName(parserStruckField).Elem().FieldByName(TestData.Field).Interface()
-			switch TestData.CheckType {
-			case CONTAINS:
-				assert.Contains(
-					t,
-					filedForCheck,
-					TestData.ExpectedResult,
-					"failed on "+parserName+"\n"+
-						"structure "+parserStruckField+"."+TestData.Field+"\n"+
-						"path to input file: "+pathToInput,
-				)
-			case EQUAL:
-				assert.Equal(
-					t,
-					TestData.ExpectedResult,
-					filedForCheck,
-					"failed on "+parserName+"\n"+
-						"structure "+parserStruckField+"."+TestData.Field+"\n"+
-						"path to input file: "+pathToInput,
-				)
-			case LEN:
-				assert.Len(
-					t,
-					filedForCheck,
-					TestData.ExpectedResult.(int),
-					"failed on "+parserName+"\n"+
-						"structure "+parserStruckField+"."+TestData.Field+"\n"+
-						"path to input file: "+pathToInput,
-				)
+	whoisRecord = parser.Parse(text)
+	whoisRecordReflect = reflect.ValueOf(whoisRecord).Elem()
+	for fieldName, assertParamsList := range assertParamsMap {
+		for _, assertParams := range assertParamsList {
+			actualField := whoisRecordReflect.
+				FieldByName(fieldName).Elem().
+				FieldByName(assertParams.TargetField).Interface()
+			assertMsg = fmt.Sprintf(assertMsgFormat, parserName, fieldName, assertParams.TargetField, inputFilepath)
+			switch assertParams.AssertType {
+			case AssertContains:
+				assert.Contains(t, actualField, assertParams.ExpectedResult, assertMsg)
+			case AssertEqual:
+				assert.Equal(t, assertParams.ExpectedResult, actualField, assertMsg)
+			case AssertLen:
+				assert.Len(t, actualField, assertParams.ExpectedResult.(int), assertMsg)
 			}
-
 		}
 	}
 }
