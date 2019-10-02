7a340d51e840dbddadc31609efa9521e9ca52d2b
dark <dark@spyse.com>
2019-10-02 13:05:57 +0300

    Add ir parser with tests

---
+++ parser_ir.go
@@ -0,0 +1,34 @@
+package whoisparser
+
+import (
+	"regexp"
+)
+
+var irParser = &Parser{
+	lineMinLen: 5,
+
+	errorRegex: &ParseErrorRegex{
+		NoSuchDomain:     regexp.MustCompile(`no entries found`),
+		RateLimit:        nil,
+		MalformedRequest: nil, //whois result for NoSuchDomain and MalformedRequest same
+	},
+
+	registrarRegex: &RegistrarRegex{
+		DomainName:     regexp.MustCompile(`domain: *(.+)`),
+		ExpirationDate: regexp.MustCompile(`expire-date: *(.+)`),
+		NameServers:    regexp.MustCompile(`nserver: *(.+)`),
+		UpdatedDate:    regexp.MustCompile(`last-updated: *(.+)`),
+	},
+
+	registrantRegex: &RegistrantRegex{
+		Name:     regexp.MustCompile(`\(Domain Holder\) *(.+)`),
+		City:     regexp.MustCompile(`\(Domain Holder Address\)(?:.*, (.*)(?:.*, (?:.*),))`),
+		Province: regexp.MustCompile(`\(Domain Holder Address\)(?:.*, (.*),)`),
+		Country:  regexp.MustCompile(`\(Domain Holder Address\)(?:.*, (.*))`),
+		Street:   regexp.MustCompile(`\(Domain Holder Address\)(?:.*, (.*), (?:.*, (?:.*),))`),
+	},
+}
+
+func init() {
+	RegisterParser(".ir", irParser)
+}
