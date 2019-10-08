fa20170c0458f1282b2fb31938b5ac132b70a52d
dark <dark@spyse.com>
2019-10-08 11:47:19 +0300

    Add parser for 'uk' tld

---
+++ parser_uk.go
@@ -0,0 +1,34 @@
+package whoisparser
+
+import (
+	"regexp"
+)
+
+var ukParser = &Parser{
+
+	errorRegex: &ParseErrorRegex{
+		NoSuchDomain:     regexp.MustCompile(`No match for`),
+		RateLimit:        nil,
+		MalformedRequest: regexp.MustCompile(`This domain cannot be registered because it contravenes`),
+	},
+
+	registrarRegex: &RegistrarRegex{
+		CreatedDate:    regexp.MustCompile(`Relevant dates:\s *Registered on: *(.*)`),
+		DomainDNSSEC:   nil,
+		DomainID:       nil,
+		DomainName:     regexp.MustCompile(`Domain name:\s *(.*)`),
+		DomainStatus:   regexp.MustCompile(`Registration status:\s *(.*)`),
+		Emails:         nil,
+		ExpirationDate: regexp.MustCompile(`Relevant dates:(?:.*\s)+Expiry date: *(.*)`),
+		NameServers:    regexp.MustCompile(`(?s)Name servers:\s *(.*?)+?\n\n`),
+		ReferralURL:    nil,
+		RegistrarID:    nil,
+		RegistrarName:  regexp.MustCompile(`Registrar:\s *(.*)`),
+		UpdatedDate:    regexp.MustCompile(`Relevant dates:(?:.*\s)+Last updated: *(.*)`),
+		WhoisServer:    nil,
+	},
+}
+
+func init() {
+	RegisterParser(".uk", ukParser)
+}
---
+++ parser_uk_test.go
@@ -0,0 +1,68 @@
+package whoisparser
+
+import (
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"testing"
+)
+
+func TestParserUk(t *testing.T) {
+	assertParamsMap := AssertParamsMap{
+		"Registrar": {
+			{TargetField: "DomainName", ExpectedResult: "run.uk", AssertType: AssertTypeEqual},
+			{TargetField: "RegistrarName", ExpectedResult: "Lexsynergy Ltd [Tag = LEXSYNERGY]", AssertType: AssertTypeEqual},
+			{TargetField: "CreatedDate", ExpectedResult: "26-Nov-2014", AssertType: AssertTypeEqual},
+			{TargetField: "ExpirationDate", ExpectedResult: "26-Nov-2019", AssertType: AssertTypeEqual},
+			{TargetField: "UpdatedDate", ExpectedResult: "26-Jul-2019", AssertType: AssertTypeEqual},
+			{TargetField: "DomainStatus", ExpectedResult: "Registered until expiry date.", AssertType: AssertTypeEqual},
+			{TargetField: "NameServers", ExpectedResult: "ns2.uniregistrymarket.link", AssertType: AssertTypeContains},
+		},
+	}
+
+	testDataFilepath := "test_data/whois_uk/run.uk.txt"
+	parserName := ".uk parser"
+
+	testParser(t, ukParser, assertParamsMap, testDataFilepath, parserName)
+}
+
+func TestParserUkNoSuchDomainErr(t *testing.T) {
+	var fileBytes []byte
+	var err error
+	var text string
+	var whoisRecord *Record
+
+	fileBytes, err = ioutil.ReadFile("test_data/whois_uk/no_such_domain.txt")
+	assert.NoError(t, err, "failed to open file with test data")
+
+	text = string(fileBytes)
+
+	whoisRecord = ukParser.Parse(text)
+
+	assert.True(t, whoisRecord.ErrCode == ErrCodeNoSuchDomain)
+	assert.Nil(t, whoisRecord.Registrar)
+	assert.Nil(t, whoisRecord.Registrant)
+	assert.Nil(t, whoisRecord.Bill)
+	assert.Nil(t, whoisRecord.Admin)
+	assert.Nil(t, whoisRecord.Tech)
+}
+
+func TestParserUkMalformedRequest(t *testing.T) {
+	var fileBytes []byte
+	var err error
+	var text string
+	var whoisRecord *Record
+
+	fileBytes, err = ioutil.ReadFile("test_data/whois_uk/malformed_request.txt")
+	assert.NoError(t, err, "failed to open file with test data")
+
+	text = string(fileBytes)
+
+	whoisRecord = ukParser.Parse(text)
+
+	assert.True(t, whoisRecord.ErrCode == ErrCodeMalformedRequest)
+	assert.Nil(t, whoisRecord.Registrar)
+	assert.Nil(t, whoisRecord.Registrant)
+	assert.Nil(t, whoisRecord.Bill)
+	assert.Nil(t, whoisRecord.Admin)
+	assert.Nil(t, whoisRecord.Tech)
+}
