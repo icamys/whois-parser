34001d5124bf0637e7b70e2924f7fca321a7dd38
dark <dark@spyse.com>
2019-10-11 16:18:24 +0300

    Take away ".co.jp" tld parser from ".jp" parent with tests

--- parser_co_jp.go
+++ parser_co_jp.go
@@ -1,13 +1,41 @@
 package whoisparser
 
-import "regexp"
+import (
+	"regexp"
+)
 
-var coJpParser = jpParser
+var coJpParser = &Parser{
+
+	errorRegex: &ParseErrorRegex{
+		NoSuchDomain:     regexp.MustCompile(`No match!`),
+		RateLimit:        nil,
+		MalformedRequest: regexp.MustCompile(`<JPRS WHOIS HELP>`),
+	},
+
+	registrarRegex: &RegistrarRegex{
+		CreatedDate:    regexp.MustCompile(`\[Registered Date\] *(.+)`),
+		DomainName:     regexp.MustCompile(`(?i)\[Domain Name] *(.+)`),
+		DomainStatus:   regexp.MustCompile(`(?i)\[Status] *(.+)`),
+		Emails:         regexp.MustCompile(`(?i)(` + EmailRegex + `)`),
+		ExpirationDate: regexp.MustCompile(`\[State\] *(.+)`),
+		NameServers:    regexp.MustCompile(`(?i)\[Name Server] *(.+)`),
+		UpdatedDate:    regexp.MustCompile(`\[Last Update\] *(.+)`),
+	},
+
+	registrantRegex: &RegistrantRegex{
+		Name:         regexp.MustCompile(`(?i)\[Registrant] *(.+)`),
+		Organization: regexp.MustCompile(`(?i)\[Organization] *(.+)`),
+	},
+
+	adminRegex: &RegistrantRegex{
+		ID: regexp.MustCompile(`(?i)\[Administrative Contact] *(.+)`),
+	},
+
+	techRegex: &RegistrantRegex{
+		ID: regexp.MustCompile(`(?i)\[Technical Contact] *(.+)`),
+	},
+}
 
 func init() {
-	coJpParser.registrarRegex.CreatedDate = regexp.MustCompile(`\[Registered Date\] *(.+)`)
-	coJpParser.registrarRegex.ExpirationDate = regexp.MustCompile(`\[State\] *(.+)`)
-	coJpParser.registrarRegex.UpdatedDate = regexp.MustCompile(`\[Last Update\] *(.+)`)
-
 	RegisterParser(".co.jp", coJpParser)
 }
--- parser_jp.go
+++ parser_jp.go
@@ -8,7 +8,7 @@ var jpParser = &Parser{
 
 	errorRegex: &ParseErrorRegex{
 		NoSuchDomain:     regexp.MustCompile(`No match!`),
-		RateLimit:        nil,
+		RateLimit:        regexp.MustCompile(`Cannot process your search request`),
 		MalformedRequest: regexp.MustCompile(`<JPRS WHOIS HELP>`),
 	},
 
@@ -16,7 +16,7 @@ var jpParser = &Parser{
 		CreatedDate:    regexp.MustCompile(`(?i)\[Created on] *(.+)`),
 		DomainName:     regexp.MustCompile(`(?i)\[Domain Name] *(.+)`),
 		DomainStatus:   regexp.MustCompile(`(?i)\[Status] *(.+)`),
-		Emails:         regexp.MustCompile(`(?i)` + EmailRegex),
+		Emails:         regexp.MustCompile(`(?i)(` + EmailRegex + `)`),
 		ExpirationDate: regexp.MustCompile(`(?i)\[Expires on] *(.+)`),
 		NameServers:    regexp.MustCompile(`(?i)\[Name Server] *(.+)`),
 		UpdatedDate:    regexp.MustCompile(`(?i)\[Last Updated] *(.+)`),
