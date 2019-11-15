355635e9487b72470ba9b287d04b6abbfb515ceb
sergey <sergey@spyse.com>
2019-11-15 16:03:20 +0200

    added .edu parser, test, and txt to project

---
+++ parser_edu.go
@@ -0,0 +1,31 @@
+package whoisparser
+
+import (
+	"regexp"
+)
+
+var ruParser = &Parser{
+
+	errorRegex: &ParseErrorRegex{
+		NoSuchDomain:     regexp.MustCompile(`No entries found for the selected source`),
+		RateLimit:        regexp.MustCompile(`You have exceeded allowed connection rate`),
+		MalformedRequest: nil, //MalformedRequest and NoSuchDomain same
+	},
+
+	registrarRegex: &RegistrarRegex{
+		CreatedDate:    regexp.MustCompile(`(?i)created: *(.+)`),
+		DomainName:     regexp.MustCompile(`(?i)domain: *(.+)`),
+		DomainStatus:   regexp.MustCompile(`(?i)state: *(.+)`),
+		ExpirationDate: regexp.MustCompile(`(?i)free-date: *(.+)`),
+		NameServers:    regexp.MustCompile(`(?i)nserver: *(.+).`),
+		RegistrarName:  regexp.MustCompile(`(?i)registrar: *(.+)`),
+	},
+
+	registrantRegex: &RegistrantRegex{
+		Organization: regexp.MustCompile(`(?i)org: *(.+)`),
+	},
+}
+
+func init() {
+	RegisterParser(".ru", ruParser)
+}
