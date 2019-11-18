705a99d2ac926e67e755e3d8be2e4b5076e63d6c
sergey <sergey@spyse.com>
2019-11-18 12:09:04 +0200

    added parser edu to package

--- parser_edu.go
+++ parser_edu.go
@@ -4,28 +4,44 @@ import (
 	"regexp"
 )
 
-var ruParser = &Parser{
+var eduParser = &Parser{
 
 	errorRegex: &ParseErrorRegex{
-		NoSuchDomain:     regexp.MustCompile(`No entries found for the selected source`),
-		RateLimit:        regexp.MustCompile(`You have exceeded allowed connection rate`),
-		MalformedRequest: nil, //MalformedRequest and NoSuchDomain same
+		NoSuchDomain:     regexp.MustCompile(`NO MATCH`),
+		RateLimit:        nil,
+		MalformedRequest: regexp.MustCompile(`Invalid domain name`),
 	},
 
 	registrarRegex: &RegistrarRegex{
-		CreatedDate:    regexp.MustCompile(`(?i)created: *(.+)`),
-		DomainName:     regexp.MustCompile(`(?i)domain: *(.+)`),
-		DomainStatus:   regexp.MustCompile(`(?i)state: *(.+)`),
-		ExpirationDate: regexp.MustCompile(`(?i)free-date: *(.+)`),
-		NameServers:    regexp.MustCompile(`(?i)nserver: *(.+).`),
-		RegistrarName:  regexp.MustCompile(`(?i)registrar: *(.+)`),
+		CreatedDate:    regexp.MustCompile(`Domain record activated: *(.+)`),
+		DomainDNSSEC:   nil,
+		DomainID:       nil,
+		DomainName:     regexp.MustCompile(`Domain Name: *(.+)`),
+		DomainStatus:   nil,
+		Emails:         nil,
+		ExpirationDate: regexp.MustCompile(`Domain expires: *(.+)`),
+		NameServers:    regexp.MustCompile(`(?s)Name Servers:\s *(.*?)+?\n\n`),
+		ReferralURL:    nil,
+		RegistrarID:    nil,
+		RegistrarName:  nil,
+		UpdatedDate:    regexp.MustCompile(`Domain record last updated: *(.+)`),
+		WhoisServer:    regexp.MustCompile(`available at: (.*)`),
+	},
+
+	adminRegex: &RegistrantRegex{
+		Name:         regexp.MustCompile(`Administrative Contact:\n\t(.+)`),
+		Organization: regexp.MustCompile(`Administrative Contact:\n*.+\n\t(.*)`),
 	},
 
 	registrantRegex: &RegistrantRegex{
-		Organization: regexp.MustCompile(`(?i)org: *(.+)`),
+		Organization: regexp.MustCompile(`(?m)Registrant:\n\t(.*)`),
 	},
+
+	techRegex: &RegistrantRegex{
+		Organization: regexp.MustCompile(`Technical Contact:\n*.+\n\t(.*)`),
+		Name:         regexp.MustCompile(`Technical Contact:\n\t(\n*.+)`)},
 }
 
 func init() {
-	RegisterParser(".ru", ruParser)
+	RegisterParser(".edu", eduParser)
 }
