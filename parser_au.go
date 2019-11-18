074599b073fff125ab9f7bccc47c01c54be38d33
sergey <sergey@spyse.com>
2019-11-18 18:27:03 +0200

    added au-tld in package whoisparser

---
+++ parser_au.go
@@ -0,0 +1,39 @@
+package whoisparser
+
+import "regexp"
+
+var auParser = &Parser{
+	errorRegex: &ParseErrorRegex{
+		NoSuchDomain:     regexp.MustCompile(`NOT FOUND`),
+		RateLimit:        nil,
+		MalformedRequest: regexp.MustCompile(`Pattern starts with improper character`),
+	},
+
+	registrarRegex: &RegistrarRegex{
+		DomainDNSSEC:  regexp.MustCompile(`DNSSEC: (.+)`),
+		DomainID:      regexp.MustCompile(`Registry Domain ID: *(.+)`),
+		DomainName:    regexp.MustCompile(`Domain Name: *(.+)`),
+		DomainStatus:  regexp.MustCompile(`(?i)Status: +(\w+).*`),
+		Emails:        regexp.MustCompile(`Registrar Abuse Contact Email: (.*)`),
+		NameServers:   regexp.MustCompile(`(?i)Name Server: +(.+)`),
+		ReferralURL:   regexp.MustCompile(`Registrar URL: +(.*)`),
+		RegistrarName: regexp.MustCompile(`Registrar Name: *(.+)`),
+		UpdatedDate:   regexp.MustCompile(`Last Modified: *(.+)`),
+		WhoisServer:   regexp.MustCompile(`Registrar WHOIS Server: (.*)`),
+	},
+
+	registrantRegex: &RegistrantRegex{
+		Organization: regexp.MustCompile(`Registrant: (.*)`),
+		ID:           regexp.MustCompile(`Registrant Contact ID: (.*)`),
+		Name:         regexp.MustCompile(`Registrant Contact Name: (.*)`),
+	},
+
+	techRegex: &RegistrantRegex{
+		ID:   regexp.MustCompile(`Tech Contact ID: (.*)`),
+		Name: regexp.MustCompile(`Tech Contact Name: (.*)`),
+	},
+}
+
+func init() {
+	RegisterParser(".au", auParser)
+}
